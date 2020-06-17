1446257fb02f9cc65b402b713c996714210b99fd
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-17 04:07:12 -0700

    v-45 LogItem component

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,21 @@ import LogItem from './LogItem';
 const App = () => {
   const [logs, setLogs] = useState([
     {
-      id: 1,
+      _id: 1,
       text: 'This is log one',
       priority: 'low',
       user: 'Brad',
       created: new Date().toString(),
     },
     {
-      id: 2,
+      _id: 2,
       text: 'This is log two',
       priority: 'moderate',
       user: 'Kate',
       created: new Date().toString(),
     },
     {
-      id: 3,
+      _id: 3,
       text: 'This is log three',
       priority: 'high',
       user: 'John',
@@ -42,7 +42,7 @@ const App = () => {
         </thead>
         <tbody>
           {logs.map((log) => (
-            <LogItem />
+            <LogItem key={log._id} log={log} />
           ))}
         </tbody>
       </Table>
