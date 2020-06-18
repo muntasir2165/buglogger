542a6a15f94febf7f5e98739104b8d9832baafdd
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-17 22:49:35 -0700

    v-49 displaying alerts

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
+import Alert from 'react-bootstrap/Alert';
 import LogItem from './LogItem';
 import AddLogItem from './AddLogItem';
 
@@ -28,16 +29,36 @@ const App = () => {
       created: new Date().toString(),
     },
   ]);
+  const [alert, setAlert] = useState({
+    show: false,
+    message: '',
+    variant: 'success',
+  });
 
   function addItem(item) {
+    if (item.message === '' || item.user === '' || item.priority === '') {
+      showAlert('Plese enter all fields', 'danger');
+      return false;
+    }
+
     item._id = Math.floor(Math.random() * 90000) + 10000;
     item.created = new Date().toString();
     setLogs([...logs, item]);
+    showAlert('Log Added');
+  }
+
+  function showAlert(message, variant = 'success', seconds = 3000) {
+    setAlert({ show: true, message, variant });
+
+    setTimeout(() => {
+      setAlert({ show: false, message: '', variant: 'success' });
+    }, seconds);
   }
 
   return (
     <Container>
       <AddLogItem addItem={addItem} />
+      {alert.show && <Alert variant={alert.variant}>{alert.message}</Alert>}
       <Table>
         <thead>
           <tr>
